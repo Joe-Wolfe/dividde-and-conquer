0882f22d232b6be7bbd76923a194fba35128883b
Joe Wolfe <JoeWolfe0527@gmail.com>
2023-10-19 22:08:04 -0400

    finished 5th assignment

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,20 @@
-function findFloor() {
-  
+function findFloor(arr, target) {
+
+    if (arr[0] > target) return -1;
+    if (arr[arr.length - 1] < target) return arr[arr.length - 1];
+
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] > target)
+            right = mid - 1;
+        else
+            left = mid + 1;
+    }
+    return arr[left - 1];
 }
 
 module.exports = findFloor
